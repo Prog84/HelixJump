111b10cb2cc94fd16afa311d2dd6715a2841b9f6
Prog84 <bvv198424@rambler.ru>
2021-02-01 15:39:21 +1000

    Add final message

--- Assets/Scripts/Ball/Ball.cs
+++ Assets/Scripts/Ball/Ball.cs
@@ -2,11 +2,24 @@
 
 public class Ball : MonoBehaviour
 {
+    private bool iSFinish = false;
     private void OnTriggerEnter(Collider other)
     {
         if (other.TryGetComponent(out PlatformSegment platformSegment))
         {
             platformSegment.GetComponentInParent<Platform>().Break();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.TryGetComponent(out PlatformSegment platformSegment))
+        {
+            if (platformSegment.GetComponentInParent<FinishPlatform>() && !iSFinish)
+            {
+                iSFinish = true;
+                Debug.Log("Вы достигли финиша!!!");
+            }
         }
     }
 }
