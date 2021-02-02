fd4efa76ff95cacf05b25672e78596e763ec55be
Prog84 <bvv198424@rambler.ru>
2021-02-02 10:18:13 +1000

    Update Ball and PlatformSegment scripts

--- Assets/Scripts/Platform/PlatformSegment.cs
+++ Assets/Scripts/Platform/PlatformSegment.cs
@@ -3,12 +3,23 @@
 [RequireComponent(typeof(Rigidbody))]
 public class PlatformSegment : MonoBehaviour
 {
+    private bool _iSFinish = false;
+
     public void Bounce(float force, Vector3 center, float radius)
     {
         if (TryGetComponent(out Rigidbody rigidbody))
         {
             rigidbody.isKinematic = false;
             rigidbody.AddExplosionForce(force, center, radius);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.TryGetComponent(out Ball ball) && GetComponentInParent<FinishPlatform>() && !_iSFinish)
+        {
+                _iSFinish = true;
+                Debug.Log("Вы достигли финиша!!!");
         }
     }
 }
